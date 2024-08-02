5f08e8a6417afaf76b22ae635b6f698718180428
Matheus Grawe <matheus.grawe12@gmail.com>
2024-08-02 01:12:25 -0300

    fix/postcss

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
-    plugins: [
-        require('@tailwindcss/forms'),
-        require('autoprefixer'),
-    ]
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    }
 }
