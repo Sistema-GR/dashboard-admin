cd733972580401e2085b06bc79d90f7ec34158dc
Matheus Grawe <matheus.grawe12@gmail.com>
2024-08-04 19:47:31 -0300

    feat/overview

---
+++ src/components/Buttons/PrimaryButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button
+      :class="`px-2 py-4 text-white bg-lw-tasks-500 hover:bg-lw-tasks-900 rounded-xl font-bold w-full ${customColor}`"
+      v-bind="$attrs"
+  >
+    {{ value }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "PrimaryButton",
+  inheritAttrs: false,
+  props: {
+    customColor: {
+      type: String,
+      required: false,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
--- src/views/Auth/login.vue
+++ src/views/Auth/login.vue
@@ -4,9 +4,11 @@
       <div class="flex-1">
         <div class="flex flex-col h-screen items-center justify-center">
           <img src="../../assets/images/logo.png" alt="" class="w-max drop-shadow-lg py-10" />
-          <div class="w-3/6 space-y-5">
+          <div class="w-full max-w-md space-y-5 px-2">
             <TextInput type="text" label="Matrícula" placeholder="00000" />
             <TextInput type="password" label="Senha" placeholder="Senha" />
+            <router-link to="/overview"> <PrimaryButton class="mt-5" value="Login" custom-color="bg-[#06A6D8]" /></router-link>"
+
           </div>
         </div>
       </div>
@@ -18,10 +20,11 @@
 </template>
 
 <script>
-import TextInput from "@/components/TextInput.vue";
+import TextInput from "@/components/Inputs/TextInput.vue";
+import PrimaryButton from "@/components/Buttons/PrimaryButton.vue";
 
 export default {
   name: 'Login',
-  components: {TextInput},
+  components: {PrimaryButton, TextInput},
 }
 </script>
