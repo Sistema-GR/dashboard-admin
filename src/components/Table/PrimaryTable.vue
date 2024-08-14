c8668a106826006f6201c34bc537da35247e824c
Matheus Gabriel Grawe <u04453@pmjlle.joinville.sc.gov.br>
2024-08-14 13:18:11 -0300

    feat/results

---
+++ src/components/Table/PrimaryTable.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="px-4 sm:px-2 lg:px-4 w-full">
+      <div class="mt-8 flow-root">
+        <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <table class="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">Name</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Title</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Email</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Role</th>
+                  <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-3">
+                    <span class="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody class="bg-white">
+                <tr v-for="person in people" :key="person.email" class="even:bg-gray-50">
+                  <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">{{ person.name }}</td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.title }}</td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.email }}</td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.role }}</td>
+                  <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-3">
+                    <a href="#" class="text-indigo-600 hover:text-indigo-900">
+                      Edit<span class="sr-only">, {{ person.name }}</span>
+                    </a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'PrimaryTable',
+    data() {
+      return {
+        people: [
+          { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
+          { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
+          { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
+          { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
+          { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
+          { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
+          { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
+          { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
+          { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
+          // More people...
+        ]
+      }
+    }
+  }
+  </script>
+  
--- src/views/AdminPanel/Results/index.vue
+++ src/views/AdminPanel/Results/index.vue
@@ -1,13 +1,65 @@
 <template>
     <Whiteboard title="Resultados IDEM">
 
+        <div class="flex flex-col items-center justify-center py-2">
+            <p class="font-semibold text-sm lg:text-lg">Resultado da Rede</p>
+        </div>
+            
+            <div class="flex flex-col items-center justify-between text-start mt-5 gap-x-10 w-full lg:flex-row sm:gap-y-5 px-4 gap-y-5">
+                <div class="flex flex-col w-full gap-y-2"> 
+                    <p>Etapa 1</p>
+                        <TextInput
+                        placeholder="%:"
+                        v-model="textValue"
+                        :error="errorMessage"
+                        :success="isSuccess"
+                        :info="isInfo"
+                        :warning="isWarning"
+                        :disabled="true"
+                        type="text"
+                        />
+                </div> 
+
+                <div class="flex flex-col w-full gap-y-2"> 
+                    <p>Etapa 1</p>
+                        <TextInput
+                        placeholder="%:"
+                        v-model="textValue"
+                        :error="errorMessage"
+                        :success="isSuccess"
+                        :info="isInfo"
+                        :warning="isWarning"
+                        :disabled="true"
+                        type="text"
+                        />
+                </div> 
+
+                <div class="flex flex-col w-full gap-y-2"> 
+                    <p>Etapa 1</p>
+                        <TextInput
+                        placeholder="%:"
+                        v-model="textValue"
+                        :error="errorMessage"
+                        :success="isSuccess"
+                        :info="isInfo"
+                        :warning="isWarning"
+                        :disabled="true"
+                        type="text"
+                        />
+                </div> 
+            </div>
+        
+        <PrimaryTable />
     </Whiteboard>
 </template>
 
 <script>
+import TextInput from '@/components/Inputs/TextInput.vue';
+import PrimaryTable from '@/components/Table/PrimaryTable.vue';
 import Whiteboard from '@/components/Whiteboard/Whiteboard.vue';
+
 export default {
     name: "Resultados IDEM",
-    components: {Whiteboard}
+    components: {Whiteboard, PrimaryTable, TextInput}
 }
 </script>
