d1bb68bd224a03b094331d08c234e33b4456091b
Matheus Grawe <matheus.grawe12@gmail.com>
2024-10-03 08:27:11 -0300

    fix screen error

--- src/views/Auth/Register/index.vue
+++ src/views/Auth/Register/index.vue
@@ -1,35 +1,33 @@
 <template>
-    <div class="flex-1 h-screen bg-gradient-to-r from-[#003965] to-95%0% to-[#0073CB]">
-      <div class="flex flex-row">
-        <div class="flex-1">
-          <div class="flex flex-col h-screen items-center justify-center">
-            <img src="../../../assets/images/logo.png" alt="" class="w-3/12 scale-75 drop-shadow-lg py-0" />
-            <div class="w-full max-w-md space-y-3 px-5">
-              <TextInput type="text" label="Matrícula" placeholder="00000" />
-              <TextInput type="text" label="Email" placeholder="E-mail" />
-              <TextInput type="text" label="CPF" placeholder="CPF" />
-              <TextInput type="password" label="Senha" placeholder="Senha" />
-              <TextInput type="password" label="Confirmar Senha" placeholder="Confirmar Senha" />
-              <router-link to="/"> <PrimaryButton class="mt-8" value="Cadastrar" custom-color="bg-azure-500" /></router-link>"
-            <div class="w-full flex justify-center mt-1">
-              <router-link to="/" class="text-sm text-amber-50 hover:underline -translate-y-5">Possui login? Clique aqui</router-link>
-            </div>
-            </div>
-          </div>
+    <div class="min-h-screen flex flex-col lg:flex-row bg-gradient-to-r from-[#003965] to-[#0073CB]">
+        <div class="flex-1 flex flex-col justify-center items-center px-4 lg:px-0 py-8 lg:py-0">
+            <img src="../../../assets/images/logo.png" alt="Logo" class="w-1/4 lg:w-2/12 drop-shadow-lg mb-8" />
+            <div class="w-full max-w-md space-y-4 px-5">
+                <TextInput type="text" label="Matrícula" placeholder="00000" />
+                <TextInput type="text" label="Email" placeholder="E-mail" />
+                <TextInput type="text" label="CPF" placeholder="CPF" />
+                <TextInput type="password" label="Senha" placeholder="Senha" />
+                <TextInput type="password" label="Confirmar Senha" placeholder="Confirmar Senha" />
+                <router-link to="/home/overview">
+                    <PrimaryButton class="w-full mt-8 bg-blue-500 py-3" value="Login" />
+                </router-link>
+                <div class="w-full flex justify-center">
+                    <router-link to="/" class="text-sm text-amber-50 hover:underline -translate-y-5 mt-3">Possui login? Clique aqui</router-link>
+                </div>
+           </div>
         </div>
-        <div class="flex-1 relative hidden lg:block">
-          <img src="../../../assets/images/prefeitura.png" alt="" class="w-full h-screen" />
+        <div class="flex-1 hidden lg:block">
+          <img src="../../../assets/images/prefeitura.png" alt="Prefeitura" class="w-full h-full object-cover" />
         </div>
-      </div>
     </div>
-  </template>
-  
-  <script>
-  import TextInput from "@/components/Inputs/TextInput.vue";
-  import PrimaryButton from "@/components/Buttons/PrimaryButton.vue";
-  
-  export default {
-    name: 'Register',
-    components: {PrimaryButton, TextInput},
-  }
-  </script>
+</template>
+
+<script>
+import TextInput from "@/components/Inputs/TextInput.vue";
+import PrimaryButton from "@/components/Buttons/PrimaryButton.vue";
+
+export default {
+  name: 'Register',
+  components: { PrimaryButton, TextInput },
+};
+</script>
